b7d092566840539d7a3a750a8f982aada3825448
pratikpatel8486 <pratikpatel8486@gmail.com>
2022-05-23 22:06:17 +0530

    add brand modal

--- src/app/Admin/add-cars/add-cars.component.ts
+++ src/app/Admin/add-cars/add-cars.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import fileDialog from 'file-dialog'
 
 @Component({
   selector: 'app-add-cars',
@@ -10,6 +11,21 @@ export class AddCarsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  uploadPhoto() {
+    fileDialog({multiple: true, accept: 'image/*'})
+    .then((files: any) => console.log(files))
+    // .then(() => {
+    //     this.utilService.notify('Images uploaded successfully.</br>They will be displayed here shortly.');
+    //     setTimeout(() => {
+    //         if (this.albumCreate) {
+    //             this.getUploadedImagesNewAlbum();
+    //         } else {
+    //             this.getUploadedImages();
+    //         }
+    //     }, 4000);
+    // }).catch();
   }
 
 }
